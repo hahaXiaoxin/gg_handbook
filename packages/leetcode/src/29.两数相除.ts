8f2637529dd4ac90efacbe0f208ca60e687dbad1
Xiaoxin <wangzs@reolink.com.cn>
2025-07-02 09:11:36 +0800

    feat: 新增<两数相除>题解

---
+++ packages/leetcode/src/29.两数相除.ts
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=29 lang=typescript
+ *
+ * [29] 两数相除
+ */
+
+// @lc code=start
+
+const MAX_INT = Math.pow(2, 31) - 1;
+const MIN_INT = Math.pow(2, 31) * -1;
+
+function divide(dividend: number, divisor: number): number {
+    // 得到单位的位次，即10^unit
+    const flag = dividend < 0 && divisor < 0 || dividend >= 0 && divisor >= 0;
+
+    const dividendArr = Math.abs(dividend).toString().split('');
+
+    const divisorAbs = Math.abs(divisor);
+
+    let result = 0;
+
+    let minusend = 0;
+
+    while (dividendArr.length > 0) {
+        minusend = Number(minusend.toString() + dividendArr.shift());
+
+        let count = 0;
+
+        while (minusend >= divisorAbs) {
+            minusend -= divisorAbs;
+            count++;
+        }
+
+        result = Number(result.toString() + count.toString());
+
+        const tempResult = flag ? result : -result;
+        if (tempResult > MAX_INT) {
+            return MAX_INT;
+        }
+
+        if (tempResult < MIN_INT) {
+            return MIN_INT;
+        }
+    }
+
+    result = flag ? result : -result;
+
+    return result;
+};
+// @lc code=end
+
